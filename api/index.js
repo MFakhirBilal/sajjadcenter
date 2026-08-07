75beeb21ebc8991349295e7f963a3ab6dc041f47
MFakhirBilal <fakhirbilal4445@gmail.com>
2026-08-07 09:12:30 +0500

    Complete clean SajjadCenter project ready for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
 import app from '../backend/server.js';
+import { connectDB } from '../backend/config/db.js';
 
-export default app;
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
